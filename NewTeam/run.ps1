c1e898ac8789e59b861d9656d85db7530b8da7ca
Sasa <93494682+sasa-z@users.noreply.github.com>
2021-12-01 11:33:06 -0800

    Update run.ps1

--- NewTeam/run.ps1
+++ NewTeam/run.ps1
@@ -1,1 +1,11 @@
-test
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::OK
+        Body       = $Body
+    })
